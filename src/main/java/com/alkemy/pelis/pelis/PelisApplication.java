de0fb4ee3617102ecafd72cab8271e4a2fad7239
exerazor1911 <ezequielgiussani56@gmail.com>
2022-05-12 18:18:14 -0300

    initial commit

---
+++ src/main/java/com/alkemy/pelis/pelis/PelisApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.pelis.pelis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelisApplication.class, args);
+	}
+
+}
